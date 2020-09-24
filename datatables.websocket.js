095b1077df7b70373cd4d616165437229990a87f
Antti Hautaniemi <antti.hautaniemi@pispalanit.fi>
2020-09-24 21:03:00 +0300

    Yhdenmukaistettu metodien nimeämiskäytäntö

--- datatables.websocket.js
+++ datatables.websocket.js
@@ -17,9 +17,11 @@
   var DTWebsocket = function (settings, opts) {
     // Hae alustettava datatauluolio.
     this.datatable = new $.fn.DataTable.Api(settings);
+    this.dtSettings = settings;
 
     // Alusta yhteyden parametrit ja konteksti.
     this.url = settings.ajax.url;
+    this.traditional = settings.ajax.traditional;
     this.websocket = null;
     this.viestijono = null;
 
@@ -27,108 +29,201 @@
     // tai uutena funktiona, joka yhdistää annetun, kiinteän
     // dataobjektin pyyntökohtaiseen;
     // vrt. `jquery.dataTables.js`, funktio `_fnBuildAjax`.
-    var ajax = settings.ajax, ajaxData = settings.ajax.data;
+    var ajaxData = settings.ajax.data;
     if (typeof ajaxData !== "function")
-      ajaxData = function (data, settings) {
-        return $.extend(true, data, ajax.data);
+      this.ajaxData = function (data, settings) {
+        return $.extend(true, data, ajaxData);
       };
-
-    // Kääri em. datafunktio siten, että oliomuotoinen data
-    // sarjallistetaan merkkijonoksi ja tallennetaan API:in;
-    // vrt. jQuery; `src/ajax.js`, funktio `ajax`.
-    this.data = function (data, settings) {
-      data = ajaxData(data, settings);
-      if (data && typeof data !== "string")
-        data = $.param(data, ajax.traditional);
-      settings.oAjaxData = data;
-      return data;
-    };
+    else
+      this.ajaxData = ajaxData;
 
     // Aseta Ajax-datapyyntöjen käsittelyfunktio.
     settings.ajax = function (data, callback, settings) {
-      settings._websocket.kasittele_pyynto(data, callback, settings);
+      settings._websocket.pyynto(data, callback, settings);
     };
 
     // Avaa WS-yhteys.
-    this.avaa_yhteys();
+    this.avaaYhteys();
   };
 
   $.extend(DTWebsocket.prototype, {
-    virheviesti: function (virhe, tn) {
-      $.fn.DataTable.ext.internal._fnLog.apply(
-        this.datatable, [this.datatable.settings()[0], 0, virhe, tn]
+    dtKutsu: function (funktio, parametrit) {
+      // Kutsu nimettyä sisäistä DT-funktiota annetuin parametrein.
+      return $.fn.DataTable.ext.internal[funktio].apply(
+        this.datatable, parametrit
       );
     },
 
-    avaa_yhteys: function () {
-      // Avaa yhteys GET-pyynnön mukaiseen polkuun.
-      var that = this;
+    avaaYhteys: function () {
+      // Avaa yhteys `ajax.url`-asetuksen mukaiseen osoitteeseen.
       this.websocket = new WebSocket(this.url);
+
+      // Tuhoa mahdollinen aiempi viestijono.
       this.viestijono = null;
-      this.websocket.onopen = function () {
-        // Ensimmäisen kerran yhteyden muodostuksen jälkeen
-        // alustetaan viestijono ja päivitetään taulun sisältö.
-        if (that.viestijono === null) {
-          that.viestijono = {};
-          that.datatable.ajax.reload();
-        }
-      };
-      this.websocket.onmessage = function (e) {
-        // Hae käsittelyfunktio saapuvalle sanomalle
-        // `draw`-parametrin mukaan.
-        // Poista funktio suorituksen jälkeen.
-        try {
-          var sanoma = JSON.parse(e.data);
-        } catch (error) {
-          that.virheviesti('Virheellinen sanoma');
-          return;
-        }
-        if (sanoma.draw in that.viestijono) {
-          if ('error' in sanoma)
-            that.virheviesti(sanoma.error);
-          else if ('draw' in sanoma && sanoma.draw in that.viestijono)
-            that.viestijono[sanoma.draw](sanoma);
-          else
-            that.virheviesti('Virheellinen sanoma');
-          delete that.viestijono[sanoma.draw];
-        }
-      };
-      this.websocket.onclose = function (e) {
-        // Kun yhteys palvelimeen katkeaa, anna virheilmoitus
-        // ja yritä hetken kuluttua uudelleen.
-        if (that.viestijono) {
-          var pyynto = Object.values(that.viestijono)[0];
-          if (pyynto) {
-            delete that.viestijono[pyynto.draw];
-          }
-        }
-        if (! e.wasClean)
-          setTimeout(function () { that.avaa_yhteys(); }, 200);
-        else
-          setTimeout(function () { that.avaa_yhteys(); }, 50);
-      };
-      this.websocket.onerror = function (e) {
-        if (that.viestijono) {
-          var pyynto = Object.values(that.viestijono)[0];
-          if (pyynto) {
-            that.virheviesti('Virhe palvelinyhteydessä', 7);
-            delete that.viestijono[pyynto.draw];
-          }
-        }
-      };
+
+      // Aseta WS-yhteyden paluukutsut.
+      var _this = this;
+      $.extend(this.websocket, {
+        onopen: function (e) { _this.yhteysAvattu(e); },
+        onclose: function (e) { _this.yhteysSuljettu(e); },
+        onerror: function (e) { _this.yhteysvirhe(e); },
+        onmessage: function (e) { _this.vastaus(e); }
+      });
     },
 
-    kasittele_pyynto: function (data, callback, settings) {
+    yhteysAvattu: function (e) {
+      // Ensimmäisellä kerralla yhteyden muodostuksen jälkeen
+      // alustetaan viestijono ja päivitetään taulun sisältö.
+      if (this.viestijono === null) {
+        this.viestijono = {};
+        this.datatable.ajax.reload();
+      }
+    },
+
+    yhteysSuljettu: function (e) {
+      // Kun yhteys palvelimeen katkeaa, anna virheilmoitus
+      // ja yritä hetken kuluttua uudelleen.
+      if (this.viestijono) {
+        var pyynto = Object.values(this.viestijono)[0];
+        if (pyynto) {
+          delete this.viestijono[pyynto.draw];
+        }
+      }
+      var _this = this;
+      if (e.code > 1001)
+        setTimeout(function () { _this.avaaYhteys(); }, 200);
+    },
+
+    yhteysvirhe: function (e) {
+      this.dtKutsu("_fnCallbackFire", [
+        this.dtSettings, null, "xhr", [
+          this.dtSettings, null, this.dtSettings.jqXHR
+        ]
+      ]);
+      if (this.viestijono) {
+        var pyynto = Object.values(this.viestijono)[0];
+        if (pyynto) {
+          this.dtKutsu("_fnLog", [
+            this.dtSettings, 0, "Virhe palvelinyhteydessä", 7
+          ]);
+          delete this.viestijono[pyynto.draw];
+        }
+      }
+    },
+
+    pyyntodata: function (data, settings) {
+      // Sarjallista data tarvittaessa merkkijonoksi.
+      // vrt. jQuery; `src/ajax.js`, funktio `ajax`.
+      data = this.ajaxData(data, settings);
+      if (data && typeof data !== "string")
+        data = $.param(data, this.traditional);
+      // Korvataan `_fnBuildAjax`-funktion asettama `oAjaxData`
+      // asetusten mukaisesti täydennetyllä datalla.
+      // Lisäksi suoritetaan mahdolliset `preXhr`-paluukutsut
+      // uudelleen tällä täydennetyllä datalla.
+      settings.oAjaxData = data;
+      this.dtKutsu("_fnCallbackFire", [
+        this.dtSettings, null, "preXhr", [
+          this.dtSettings, data
+        ]
+      ]);
+      return data;
+    },
+
+    pyynto: function (data, callback, settings) {
       // Mikäli Websocket-yhteys ei ole vielä valmis, odotetaan.
       if (! this.viestijono)
         return;
 
-      // Tyhjennetään mahdolliset aiemmat, edelleen käynnissä
-      // olevat pyynnöt. Jäädään odottamaan paluusanomaa.
-      this.viestijono = {[data.draw]: callback};
+      var _this = this;
+      var _callback = function(data) {
+        // Kääri paluukutsu uuteen funktioon,
+        // joka suorittaa ensin mahdolliset `xhr`-paluukutsut.
+        // Vrt. `jquery.dataTables.js`, funktio `_fnBuildAjax`,
+        // sisempi funktio `baseAjax.success`.
+        if ("error" in data)
+          // Virhe datapyynnön käsittelyssä: ilmoita.
+          _this.dtKutsu("_fnLog", [
+            _this.dtSettings, 0, data.error
+          ]);
+        _this.dtSettings.json = data;
+        callback(data);
+      }
 
-      // Lähetetään pyyntödata URL-koodattuna merkkijonona.
-      this.websocket.send(this.data(data, settings));
+      // Poimi datatableksen asettama pyynnön tunniste.
+      var draw = data.draw;
+
+      // Muodostetaan todellinen lähtevä dataolio täydennettynä
+      // mahdollisisilla `settings.ajax.data`-parametreillä,
+      // Homaa, että funktio myös korvaa Datatableksen asettaman,
+      // puutteellisen `settings.oAjaxData`-määreen.
+      data = this.pyyntodata(data, settings);
+      if (this.reaaliaikainen_sanoma)
+        // Käytä ensisijaisesti mahdollista palvelimen jo lähettämää,
+        // reaaliaikaista sanomaa.
+        _callback($.extend(
+          true, this.reaaliaikainen_sanoma, {draw: draw}
+        ));
+
+      else {
+        // Tyhjennetään mahdolliset aiemmat, edelleen käynnissä
+        // olevat pyynnöt. Jäädään odottamaan paluusanomaa.
+        this.viestijono = {[draw]: _callback};
+
+        // Lähetetään pyyntödata URL-koodattuna merkkijonona.
+        this.websocket.send(data);
+      }
+    },
+
+    vastaus: function (e) {
+      try {
+        var sanoma = JSON.parse(e.data);
+      } catch (error) {
+        // Sanoma ei ole JSON-muodossa; ilmoitetaan virheestä
+        // ja ohitetaan kaikki käsittely.
+        return this.dtKutsu("_fnLog", [
+          this.dtSettings, 0, "Virheellinen sanoma", 1
+        ]);
+      }
+
+      // Vain `draw`-avaimen sisältävät sanomat
+      // käsitellään tässä.
+      if ("draw" in sanoma) {
+        if (sanoma.draw === 0) {
+          // Reaaliaikainen sanoma; tallenna ja päivitä taulu.
+          // Huomaa, että `reload`-kutsu lataa tässä asetetun sanoman.
+          this.reaaliaikainen_sanoma = sanoma;
+          this.datatable.ajax.reload();
+          // Käytä kutakin reaaliaikaista sanomaa vain kerran.
+          delete this.reaaliaikainen_sanoma;
+          return;
+        }
+        else if (sanoma.draw in this.viestijono) {
+          // Vastaus aiempaan datapyyntöön. Kutsu tallennettua
+          // paluukutsufunktiota ja poista sitten pyyntö.
+          this.viestijono[sanoma.draw](sanoma);
+          delete this.viestijono[sanoma.draw];
+          return;
+        }
+        else
+          // Sanoma poikkeaa pyyntöjärjestyksestä;
+          // ilmoita JS-konsolin kautta.
+          // Huomaa, että tämä voi johtua verkkoyhteydestä johtuvista
+          // tekijöistä, ei välttämättä virhetilanteesta.
+          console.log("Sanoma ei vastaa pyyntöä:", sanoma.draw);
+      }
+
+      // Jos sanoma ei sisältänyt pyydettyä dataa, suoritetaan
+      // silti mahdolliset `xhr`-paluukutsut.
+      // Huomaa, että normaalin sanoman yhteydessä `callback`-
+      // funktio tekee tämän.
+      this.dtSettings.json = sanoma;
+      this.dtKutsu("_fnCallbackFire", [
+        this.dtSettings, null, "xhr", [
+          this.dtSettings, sanoma, this.dtSettings.jqXHR
+        ]
+      ]);
     }
   });
+
 }(jQuery));
